73ce942eff04d92275dfb271e3fc526af23836b0
ADMIN <ADMIN@LAB1-MAY33>
2024-02-26 08:24:03 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, tong;
+	cout << "CHUONG TRINH TINH CONG\N";
+	cout << "Nhap so thu nhat:";
+	cin >> so1;
+	cout << "Nhap so thu hai"; 
+	cin >> so2;
+	tong = so1 + so2;
+	cout << so1 << "+" << so2 << "=" << tong << endl;
+	return 0;
+	}
+
+
+
